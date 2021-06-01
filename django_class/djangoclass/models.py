a6c6120f1b430557c87c7a27dced096896f978f4
Yosxquisite <yetundesalami23@gmail.com>
2021-06-01 04:48:15 +0100

    Main

---
+++ django_class/djangoclass/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title =  models.CharField(max_length=30)
+    Body = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
---
+++ django_class/djangoclass/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+     posts = Post.objects.all()
+
+     
+     context = {
+        "posts" : posts
+     }
+     return render(request, "djangoclass/index.html", context)
+
+
